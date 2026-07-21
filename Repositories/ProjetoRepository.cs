7a47fa46ee097afa5b7bd1d7ff76d02f5a10c068
jlucascaraujo <jlucascaraujo@gmail.com>
2026-07-21 17:09:44 -0300

    Implement CRUD operations for Projeto entity in ProjetosController and ProjetoRepository

--- Repositories/ProjetoRepository.cs
+++ Repositories/ProjetoRepository.cs
@@ -19,6 +19,36 @@ namespace Exo.WebApi.Repositories
         {
             return _context.Projetos.ToList();
         }
-    }
 
+        public void Cadastrar(Projeto projeto)
+        {
+            _context.Projetos.Add(projeto);
+            _context.SaveChanges();
+        }
+
+        public Projeto BuscarporId(int id)
+        {
+            return _context.Projetos.Find(id);
+        }
+
+        public void Atualizar(int id, Projeto projeto)
+        {
+            Projeto projetoBuscando = _context.Projetos.Find(id);
+            if (projetoBuscando != null)
+            {
+                projetoBuscando.NomeDoProjeto = projeto.NomeDoProjeto;
+                projetoBuscando.Area = projeto.Area;
+                projetoBuscando.Status = projeto.Status;
+            }
+            _context.Projetos.Update(projetoBuscando);
+            _context.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            Projeto projetoBuscando = _context.Projetos.Find(id);
+            _context.Projetos.Remove(projetoBuscando);
+            _context.SaveChanges();
+        }
+    }
 }
